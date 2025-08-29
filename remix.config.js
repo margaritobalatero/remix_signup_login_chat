8896830a272c37d179fdd108ccc32060d380d6b7
margaritobalatero <margaritobalatero@gmail.com>
2025-08-29 22:12:02 +0800

    ok

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // Netlify specific settings:
+  serverModuleFormat: "cjs", // Use CJS for Netlify compatibility
+  serverBuildPath: "build/index.js",
   publicPath: "/build/",
   assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
 };
